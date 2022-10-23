eaef2da639fc37d3e85128d0b09dd39597cf4ad5
Anna <anna-theresia.dersch@iem.thm.de>
2022-10-23 20:14:06 +0200

    First Steps for the Sudoku, Frontend Works, some Problems in the Backend, perhaps an Version Conflict with old Variants of Code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.sudoku4x4)
+
+
     }
+
 }
---
+++ app/src/main/java/com/example/myapplication/sudoku/view/PlaySudokuActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication.sudoku.view
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+import com.example.myapplication.sudoku.game.SudokuGame
+import com.example.myapplication.sudoku.view.custom.SudokuBoardView
+import com.example.myapplication.sudoku.viewModel.PlaySudokuViewModel
+
+
+class PlaySudokuActivity : AppCompatActivity(), SudokuBoardView.OnTouchListener {
+    private lateinit var viewModel: PlaySudokuViewModel
+    private lateinit var sudokuBoardView:SudokuBoardView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_sudoku)
+
+        sudokuBoardView.registerListener(this)
+
+        viewModel = ViewModelProviders.of(this).get(PlaySudokuViewModel::class.java)
+        viewModel.sudokuGame.gewaehlteZellenLiveData.observe(this, Observer { updategewaehlteZelleUI(it) })
+
+    }
+
+    private fun updategewaehlteZelleUI(cell: Pair<Int, Int>?) = cell?.let {
+        sudokuBoardView.updategewaelteZelleUI(cell.first, cell.second)
+    }
+
+    fun gewaelteZelle(zeile: Int, spalte: Int) {
+        viewModel.sudokuGame.gewaehlteZelleUpdaten(zeile,spalte)
+
+    }
+}
+
+private fun <T> MutableLiveData<T>.observe(playSudokuActivity: PlaySudokuActivity, observer: Observer<T>) {
+
+}
+
