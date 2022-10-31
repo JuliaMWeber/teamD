756d8cbc17b4580fc0f85278869d7ea8fed653fc
jmwb03 <julia.melanie.weber@iem.thm.de>
2022-10-31 13:12:06 +0100

    update GamesList

--- app/src/main/java/com/example/gamecollection/GamesListActivity.kt
+++ app/src/main/java/com/example/gamecollection/GamesListActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gamecollection
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,45 +14,60 @@ class GamesListActivity : AppCompatActivity() {
     //var adapter: Adapter? = null
     //var nameList: ArrayList<Item>()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGamesListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val imageId = intArrayOf(
-            R.drawable.test, R.drawable.wordle
+            R.drawable.test, R.drawable.wordle, R.drawable.test
+
         )
         val name = arrayOf(
             "Sudoko",
-            "Woorlde",
-            "TicTacToe"
+            "Wordle",
+            "Tic Tac Toe"
         )
+        /*
+        val activity = arrayOf(
+            SudokoActivity, TicTacToeActivity, WordleActivity
+        )
+        */
+
         gamesArrayList = ArrayList()
         for (i in name.indices){
-            val games = Games(name[i], imageId[i])
+            val games = Games(name[i], imageId[i] )
             gamesArrayList.add(games)
         }
         binding.listview.isClickable=true
         binding.listview.adapter = MyAdapter(this,gamesArrayList)
-        binding.listview.setOnItemClickListener{parent,view,position,id ->
+        binding.listview.setOnItemClickListener {
+                parent,view,position,id ->
+
             val name = name[position]
             val imageId = imageId[position]
 
-            val i = Intent(this, TicTacToeActivity::class.java)
-            i.putExtra("name",name)
-            i.putExtra("imageId",imageId)
-            startActivity(i)
+            if(name=="Sudoko") {
+                val i = Intent(this, SudokoActivity::class.java)
+                i.putExtra("name",name)
+                i.putExtra("imageId",imageId)
+                startActivity(i)
+            }
+            if(name=="Tic Tac Toe") {
+                val i = Intent(this, TicTacToeActivity::class.java)
+                i.putExtra("name",name)
+                i.putExtra("imageId",imageId)
+                startActivity(i)
+            }
+            /*
+            if(name=="Wordle") {
+                val i = Intent(this, WordleActivity::class.java)
+                i.putExtra("name",name)
+                i.putExtra("imageId",imageId)
+                startActivity(i)
+            }
+             */
         }
-/*
-        nameList.add(Item("Android", R.drawable.test))
-        nameList.add(Item("Android", R.drawable.test))
-
-        adapter = Adapter(nameList, this)
-        gridView.adapter = adapter
-        gridView.setOnItemClickListener{adapterView, view, i, ->
-            Toast.makeText(this,"Item Selected", Toast.LENGTH_LONG).show()
-        }
-        */
-
     }
 }
